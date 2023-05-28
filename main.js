034705182d3a60a3962a5c115a1c9d27fae03692
Duy Tran <duy88tran@mail.com>
2023-05-28 10:32:39 +0700

    #3-basic train

--- main.js
+++ main.js
@@ -1,33 +1,70 @@
 //tạo ra app
 const app = Vue.createApp({
-  template: `
-    <h1>
-      Hello from {{teamName}} and {{isHello ? firstName + ' ' + lastName : ''}}
-    </h1>
-    <button v-on:click="changeName">Change Name</button>
-    <button @click="changeName">Change Name Shorter</button>
-    <div class="modal" v-if="isShowModal">
-      This is modal header
-    </div>
-  `,
-
   data() {
     return {
-      teamName: "RHP team",
-      firstName: "Duy",
-      lastName: "Tran",
-      isHello: true,
-      isShowModal: false,
+      mouseMove: {
+        x: 0,
+        y: 0,
+      },
+      productList: [
+        {
+          _id: 1,
+          class: "image-thumbnail",
+          image: "https://gaxa.vn/wp-content/uploads/2022/06/modern-house.jpg",
+          name: "House",
+          price: 300000,
+          isChosen: true,
+        },
+        {
+          _id: 2,
+          class: "image-thumbnail",
+          image:
+            "https://www.sanxehot.vn/files/1200x/audi-q3-2013-pics-301580-21WUQqav23.jpg",
+          name: "Audi",
+          price: 45000,
+          isChosen: false,
+        },
+        {
+          _id: 3,
+          class: "image-thumbnail",
+          image:
+            "https://www.coffeebeancompany.co.uk/app/uploads/2017/04/Coffee-Shop-1024x765.jpg",
+          name: "Shop",
+          price: 200000,
+          isChosen: false,
+        },
+        {
+          _id: 4,
+          class: "image-thumbnail",
+          image: "https://i.ytimg.com/vi/nJNc4HjiVsg/maxresdefault.jpg",
+          name: "Aqua Tank",
+          price: 3000,
+          isChosen: true,
+        },
+      ],
     };
   },
 
   methods: {
-    changeName() {
-      this.firstName = "AS";
-      this.lastName = "Rome";
-      this.isShowModal = !this.isShowModal;
+    onLogEvent(event) {
+      console.log("trigger", event);
+    },
+
+    onMouseMoveEvent(event) {
+      this.mouseMove.x = event.offsetX;
+      this.mouseMove.y = event.offsetY;
+    },
+
+    onChosenItem(product) {
+      product.isChosen = !product.isChosen;
     },
   },
+
+  computed: {
+    luxuryProductList() {
+      return this.productList.filter(product => product.price > 100000);
+    }
+  }
 });
 
 app.mount("#contact"); //đưa app vào thẻ có id="contact"
